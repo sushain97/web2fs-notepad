f4026459d0340322010622d74d7ab92ac2211706
Sushain Cherivirala <sushain@skc.name>
2018-08-16 23:00:34 -0700

    Run phpcs on public PHP as well

--- public/index.php
+++ public/index.php
@@ -9,7 +9,10 @@ require __DIR__.'/../vendor/autoload.php';
 
 if (!isset($_SERVER['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+        throw new \RuntimeException(
+            'APP_ENV environment variable is not defined. You need to define environment variables for configuration '.
+            'or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.'
+        );
     }
     (new Dotenv())->load(__DIR__.'/../.env');
 }
@@ -24,7 +27,10 @@ if ($debug) {
 }
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+    Request::setTrustedProxies(
+        explode(',', $trustedProxies),
+        Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
+    );
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
