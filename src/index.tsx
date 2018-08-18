d154b4ed6d466c1e532e932289ad1eefe1d0806e
Sushain Cherivirala <sushain@skc.name>
2018-08-18 15:19:05 -0700

    Store current content independently

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ import * as store from 'store/dist/store.modern'; // tslint:disable-line no-subm
 // We want to ensure that versions are somewhat meaningful by debouncing
 // updates. However, we don't want to allow lots of unsent input to get
 // built up so we only buffer UPDATE_MAX_WAIT_MS of updates.
-const UPDATE_DEBOUNCE_MS = 2500;
+const UPDATE_DEBOUNCE_MS = 2000;
 const UPDATE_MAX_WAIT_MS = 10000;
 
 interface INote {
@@ -32,6 +32,7 @@ interface IAppProps {
 }
 
 interface IAppState {
+  content: string;
   currentVersion: number;
   note: INote;
   updating: boolean;
@@ -47,8 +48,8 @@ class App extends React.Component<IAppProps, IAppState> {
   private updateNote = debounce(
     async () => {
       try {
-        const { note } = this.state;
-        const { id, content } = note;
+        const { note, content } = this.state;
+        const { id } = note;
 
         if (this.cancelTokenSource) {
           this.cancelTokenSource.cancel();
@@ -68,7 +69,7 @@ class App extends React.Component<IAppProps, IAppState> {
         this.cancelTokenSource = null;
         this.setState({
           currentVersion: updatedNote.version,
-          note: { ...updatedNote, content: this.state.note.content },
+          note: updatedNote,
           updating: false,
         });
         window.history.pushState(null, '', `/${id}/${updatedNote.version}`);
@@ -98,6 +99,7 @@ class App extends React.Component<IAppProps, IAppState> {
 
     const { note, currentVersion } = props;
     this.state = {
+      content: note.content,
       currentVersion,
       note,
       updating: false,
@@ -115,8 +117,13 @@ class App extends React.Component<IAppProps, IAppState> {
   }
 
   public render() {
-    const { note, currentVersion, updating } = this.state;
-    const disabled = note.version !== currentVersion;
+    const {
+      note: { version, modificationTime },
+      currentVersion,
+      updating,
+      content,
+    } = this.state;
+    const disabled = version !== currentVersion;
 
     // TODO: access to old versions via interactive tag?
     return (
@@ -124,7 +131,7 @@ class App extends React.Component<IAppProps, IAppState> {
         <TextArea
           inputRef={this.contentRefHandler}
           intent={Intent.PRIMARY}
-          value={note.content}
+          value={content}
           title={disabled ? 'Editing a prior version of a note is not permitted.' : ''}
           onChange={disabled ? undefined : this.handleContentChange}
           onKeyDown={disabled ? undefined : this.handleContentKeyDown}
@@ -139,11 +146,11 @@ class App extends React.Component<IAppProps, IAppState> {
             minimal={true}
             large={true}
           >
-            Version {note.version} of {currentVersion}
+            Version {version} of {currentVersion}
           </Tag>
           <Callout intent={disabled ? Intent.WARNING : undefined} className="status-bar-callout">
             {disabled && <H5>Editing disabled for old version</H5>}
-            Last modified {new Date(note.modificationTime * 1000).toLocaleString()}
+            Last modified {new Date(modificationTime * 1000).toLocaleString()}
           </Callout>
         </div>
       </>
@@ -167,7 +174,9 @@ class App extends React.Component<IAppProps, IAppState> {
   };
 
   private handleBeforeUnload = ev => {
-    if (this.state.updating) {
+    const { updating, note, content } = this.state;
+
+    if (updating || content !== note.content) {
       const message = 'Are you sure you want to leave this page with unsaved changes?';
       ev.returnValue = message;
       return message;
@@ -176,7 +185,7 @@ class App extends React.Component<IAppProps, IAppState> {
 
   private handleContentChange = (event: React.FormEvent<HTMLTextAreaElement>) => {
     const { value: content } = event.currentTarget;
-    this.setState({ note: { ...this.state.note, content } }, this.updateNote);
+    this.setState({ content }, this.updateNote);
   };
 
   private handleContentKeyDown = (ev: React.KeyboardEvent<HTMLTextAreaElement>) => {
