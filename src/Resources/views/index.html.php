4ab203a81fe8cf42f100fd67b99d8313d494c0ef
Sushain Cherivirala <sushain@skc.name>
2018-08-20 17:09:21 -0500

    Add favicon (fixes #7)

--- src/Resources/views/index.html.php
+++ src/Resources/views/index.html.php
@@ -6,7 +6,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="author" content="Sushain K. Cherivirala">
-        <link rel="stylesheet" href="<?php echo $kernel->getAssetPath('main.css') ?>"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?b69c924d943b330a0c691a38d590e091bc9863c3" />
+        <link rel="stylesheet" href="<?php echo $kernel->getAssetPath('main.css') ?>" />
         <script type="text/javascript">
             window.CONTEXT = {
                 note: <?php echo json_encode($note) ?>,
