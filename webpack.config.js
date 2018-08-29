bbc39483a51acb252861cd58848912e7398c4b2f
Sushain Cherivirala <sushain@skc.name>
2018-08-28 22:34:50 -0500

    Run prettier on more files

--- webpack.config.js
+++ webpack.config.js
@@ -12,68 +12,67 @@ const SRC_PATH = path.resolve(__dirname, 'src', 'scripts');
 const ASSETS_PATH = path.resolve(__dirname, 'public', 'assets');
 
 module.exports = {
-    mode: process.env.APP_ENV == 'dev' ? 'development' : 'production',
-    entry: fs.readdirSync(SRC_PATH).reduce((entrypoints, name) => {
-        if (!fs.statSync(path.join(SRC_PATH, name)).isDirectory() && name.endsWith('.tsx')) {
-            entrypoints[path.basename(name, '.tsx')] = path.join(SRC_PATH, name);
-        }
-        return entrypoints;
-    }, {}),
-    output: {
-        filename: '[name].[contenthash].bundle.js',
-        chunkFilename: '[name].[contenthash].chunk.js',
-        publicPath: '/assets/',
-        path: ASSETS_PATH,
-    },
-    devtool: 'eval',
-    resolve: {
-        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
-    },
-    module: {
-        rules: [{
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: 'ts-loader',
+  mode: process.env.APP_ENV == 'dev' ? 'development' : 'production',
+  entry: fs.readdirSync(SRC_PATH).reduce((entrypoints, name) => {
+    if (!fs.statSync(path.join(SRC_PATH, name)).isDirectory() && name.endsWith('.tsx')) {
+      entrypoints[path.basename(name, '.tsx')] = path.join(SRC_PATH, name);
+    }
+    return entrypoints;
+  }, {}),
+  output: {
+    filename: '[name].[contenthash].bundle.js',
+    chunkFilename: '[name].[contenthash].chunk.js',
+    publicPath: '/assets/',
+    path: ASSETS_PATH,
+  },
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              fallback: 'file-loader',
             },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/i,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192,
-                        fallback: 'file-loader',
-                    },
-                }],
-            }
+          },
         ],
-    },
-    performance: {
-        maxEntrypointSize: 1.5E9,
-        maxAssetSize: 1.25E9,
-    },
-    plugins: [
-        new CleanWebpackPlugin([ASSETS_PATH]),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].bundle.css',
-            path: ASSETS_PATH,
-        }),
-        new OptimizeCssAssetsPlugin({
-            cssProcessor: require('cssnano'),
-            cssProcessorOptions: {
-                discardComments: {
-                    removeAll: true,
-                },
-            },
-            canPrint: true,
-        }),
-        new WebpackAssetsManifest(),
+      },
     ],
+  },
+  performance: {
+    maxEntrypointSize: 1.5e9,
+    maxAssetSize: 1.25e9,
+  },
+  plugins: [
+    new CleanWebpackPlugin([ASSETS_PATH]),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].bundle.css',
+      path: ASSETS_PATH,
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true,
+        },
+      },
+      canPrint: true,
+    }),
+    new WebpackAssetsManifest(),
+  ],
 };
