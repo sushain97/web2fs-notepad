5dac85f0067f6fada96a67c2d73345044da0b0d0
Sushain Cherivirala <sushain@skc.name>
2020-01-20 14:39:18 -0800

    Tree shake icons (#48)
    * Initial try
    
    * Cleanup
    
    * Faster incremental builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,64 @@
 require('dotenv').config();
 
+const util = require('util');
 const path = require('path');
 const fs = require('fs');
+const child_process = require('child_process');
+const tmp = require('tmp');
 
+const { NormalModuleReplacementPlugin } = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
 const WebpackRequireFrom = require('webpack-require-from');
 
+const icons = require('@blueprintjs/icons');
+
 const SRC_PATH = path.resolve(__dirname, 'src', 'scripts');
 const ASSETS_PATH = path.resolve(__dirname, 'public', 'assets');
+const iconsFile = tmp.fileSync();
+
+class BlueprintIconShakingPlugin {
+  static extraIcons = ['key-command', 'key-option'];
+
+  apply(compiler) {
+    const exec = util.promisify(child_process.exec);
+    const writeFile = fs.promises.writeFile;
+
+    const iconsShaker = async _ => {
+      const usedIcons = new Set([
+        ...(await exec(`grep -ohER 'IconNames.[A-Z_]+' ${SRC_PATH}`)).stdout
+          .split('\n')
+          .map(i => i.split('.', 2)[1])
+          .filter(Boolean)
+          .map(i => i.toLowerCase().replace('_', '-')),
+        ...BlueprintIconShakingPlugin.extraIcons,
+      ]);
+
+      const iconSvgPaths16 = Object.fromEntries(
+        Object.entries(icons.IconSvgPaths16).filter(([iconName, _]) => usedIcons.has(iconName)),
+      );
+      const iconSvgPaths20 = Object.fromEntries(
+        Object.entries(icons.IconSvgPaths20).filter(([iconName, _]) => usedIcons.has(iconName)),
+      );
+
+      await writeFile(
+        iconsFile.name,
+        `export const IconSvgPaths16 = ${JSON.stringify(iconSvgPaths16, null, 2)}
+    export const IconSvgPaths20 = ${JSON.stringify(iconSvgPaths20, null, 2)}`,
+      );
+    };
+
+    compiler.hooks.beforeRun.tapPromise('BlueprintIconShakingPlugin', iconsShaker);
+    compiler.hooks.watchRun.tapPromise('BlueprintIconShakingPlugin', iconsShaker);
+  }
+}
+
+const development = process.env.APP_ENV == 'dev';
 
 module.exports = {
-  mode: process.env.APP_ENV == 'dev' ? 'development' : 'production',
+  mode: development ? 'development' : 'production',
   entry: fs.readdirSync(SRC_PATH).reduce((entrypoints, name) => {
     if (!fs.statSync(path.join(SRC_PATH, name)).isDirectory() && name.endsWith('.tsx')) {
       entrypoints[path.basename(name, '.tsx')] = path.join(SRC_PATH, name);
@@ -74,19 +119,22 @@ module.exports = {
       filename: '[name].[contenthash].bundle.css',
       path: ASSETS_PATH,
     }),
-    new OptimizeCssAssetsPlugin({
-      cssProcessor: require('cssnano'),
-      cssProcessorOptions: {
-        discardComments: {
-          removeAll: true,
+    !development &&
+      new OptimizeCssAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: {
+          discardComments: {
+            removeAll: true,
+          },
         },
-      },
-      canPrint: true,
-    }),
+        canPrint: true,
+      }),
     new WebpackAssetsManifest(),
     new WebpackRequireFrom({
       replaceSrcMethodName: 'mungeImportScriptsUrl',
       suppressErrors: true,
     }),
-  ],
+    new NormalModuleReplacementPlugin(/.*\/generated\/iconSvgPaths.*/, iconsFile.name),
+    new BlueprintIconShakingPlugin(),
+  ].filter(Boolean),
 };
