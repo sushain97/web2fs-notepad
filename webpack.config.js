59c166e3e4d336ec5e40ab454143d30214a1a844
Sushain Cherivirala <sushain@skc.name>
2021-12-29 01:52:03 -0600

    Bump webpack-asset-manifest

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ const WebpackRequireFrom = require('webpack-require-from');
 const icons = require('@blueprintjs/icons');
 
 const SRC_PATH = path.resolve(__dirname, 'src', 'scripts');
-const ASSETS_PATH = path.resolve(__dirname, 'public', 'assets');
 const iconsFile = tmp.fileSync();
 
 class BlueprintIconShakingPlugin {
@@ -69,7 +68,7 @@ module.exports = {
     filename: '[name].[contenthash].bundle.js',
     chunkFilename: '[name].[contenthash].chunk.js',
     publicPath: '/assets/',
-    path: ASSETS_PATH,
+    path: path.resolve(__dirname, 'public', 'assets'),
     globalObject: 'this',
   },
   devtool: development ? 'cheap-eval-source-map' : false,
